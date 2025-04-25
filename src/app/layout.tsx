be2286651028d171b917d1d7fa063e2b8c455b14
hhl206 <hhl206@qq.com>
2025-04-25 14:08:24 +0800

    更新布局组件，确保子组件在主内容区域内渲染；调整票务页面样式以增加底部间距

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <main className="py-6 px-6">{children}</main>
       </body>
     </html>
   );
--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -10,7 +10,7 @@ const TicketsPage = () => {
     <div>
       {tickets.map((ticket) => {
         return (
-          <div key={ticket.id} className="border-2 border-gray-300 p-4 rounded-md shadow-md">
+          <div key={ticket.id} className="border-2 border-gray-300 p-4 rounded-md shadow-md mb-2">
             <h2>{ticket.title}</h2>
             <p>{ticket.content}</p>
             <Link href={ticketPath(ticket.id)} className="underline">
