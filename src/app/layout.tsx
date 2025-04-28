68e255a5c3b446b913460467fb5ebd7b6485d9fd
hhl206 <hhl206@qq.com>
2025-04-28 17:00:58 +0800

    重构布局组件，优化导航链接样式，使用按钮变体替代文本样式

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,12 @@
 import "./globals.css";
 
+import { Variable } from "lucide-react";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Link from "next/link";
 
+import { buttonVariants } from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
 import { homePath, ticketsPath } from "@/paths";
 
 const geistSans = Geist({
@@ -33,12 +36,18 @@ export default function RootLayout({
       >
         <nav className="supports-backdrop-blur:bg-background/60 fixed left-0 right-0 top-0 z-20 border-b bg-background/95 backdrop-blur w-full flex py-2.5 px-5 justify-between">
           <div>
-            <Link className="text-lg font-bold" href={homePath()}>
+            <Link
+              href={homePath()}
+              className={buttonVariants({ variant: "outline" })}
+            >
               Home
             </Link>
           </div>
           <div>
-            <Link className="text-sm underline" href={ticketsPath()}>
+            <Link
+              className={buttonVariants({ variant: "outline" })}
+              href={ticketsPath()}
+            >
               Tickets
             </Link>
           </div>
