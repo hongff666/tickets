60302eb51faf73c3d545727e8c62d4bfba710c5f
hhl206 <hhl206@qq.com>
2025-04-30 09:31:04 +0800

    新增主题切换功能，添加 ThemeProvider 和 ThemeSwitcher 组件以支持主题切换；更新布局以集成新组件

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,13 +3,13 @@ import Link from "next/link";
 
 import { homePath, ticketsPath } from "@/paths";
 
+import { ThemeSwitcher } from "./theme/theme-switcher";
 import { buttonVariants } from "./ui/button";
-
 
 const Header = () => {
   return (
     <nav className="supports-backdrop-blur:bg-background/60 fixed left-0 right-0 top-0 z-20 border-b bg-background/95 backdrop-blur w-full flex py-2.5 px-5 justify-between">
-      <div>
+      <div className="flex items-center  gap-x-2">
         <Link
           href={homePath()}
           className={buttonVariants({ variant: "ghost" })}
@@ -18,7 +18,8 @@ const Header = () => {
           <h1 className="ml-2 text-lg font-semibold">TicketBounty</h1>
         </Link>
       </div>
-      <div>
+      <div className="flex items-center gap-x-2">
+        <ThemeSwitcher />
         <Link
           className={buttonVariants({ variant: "default" })}
           href={ticketsPath()}
