ce814f43b6760f7ffd2b463d326e94748dcc0497
hhl206 <hhl206@qq.com>
2025-05-02 20:06:49 +0800

    更新 Prisma 客户端配置，添加查询日志；新增删除票据功能，并在 TicketItem 组件中集成删除按钮

--- src/features/ticket/components/ticket-item.tsx
+++ src/features/ticket/components/ticket-item.tsx
@@ -1,12 +1,15 @@
+"use client";
+
 import { Ticket } from "@prisma/client";
 import clsx from "clsx";
-import { LucideExternalLink } from "lucide-react";
+import { LucideExternalLink, LucideTrash } from "lucide-react";
 import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { ticketPath } from "@/paths";
 
+import { deleteTicket } from "../actions/delete-ticket";
 import { TICKET_ICONS } from "../constants";
 
 type TicketItemProps = {
@@ -20,6 +23,16 @@ const TicketItem = ({ ticket, isDetail = false }: TicketItemProps) => {
       <Link href={ticketPath(ticket.id)}>
         <LucideExternalLink className="w-4 h-4" />
       </Link>
+    </Button>
+  );
+
+  const handleDeleteTicket = async () => {
+    await deleteTicket(ticket.id);
+  };
+
+  const deleteButton = (
+    <Button variant="outline" size="icon" onClick={handleDeleteTicket}>
+      <LucideTrash className="w-4 h-4" />
     </Button>
   );
 
@@ -47,9 +60,10 @@ const TicketItem = ({ ticket, isDetail = false }: TicketItemProps) => {
           </p>
         </CardContent>
       </Card>
-      {isDetail ? null : (
-        <div className="flex flex-col gap-y-2">{detailButton}</div>
-      )}
+
+      <div className="flex flex-col gap-y-2">
+        {isDetail ? deleteButton : detailButton}
+      </div>
     </div>
   );
 };
