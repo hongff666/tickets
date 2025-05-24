312d61601bdffcc29b52b363c9df5b32bde8d136
hhl206 <hhl206@qq.com>
2025-05-24 19:19:39 +0800

    优化 RedirectToast 组件，增加状态管理以避免重复显示通知，改进 cookie 操作逻辑

--- src/components/redirect-toast.tsx
+++ src/components/redirect-toast.tsx
@@ -1,19 +1,23 @@
 'use client'
 
 import { deleteCookieByKey, getCookieByKey } from '@/actions/cookies'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { toast } from 'sonner'
 
 const RedirectToast = () => {
+  const [hasShownToast, setHasShownToast] = useState(false)
+
   useEffect(() => {
-    getCookieByKey('toast').then((message) => {
+    if (hasShownToast) return
+    ;(async () => {
+      const message = await getCookieByKey('toast')
       if (message) {
-        deleteCookieByKey('toast').then(() => {
-          toast.success(message)
-        })
+        await deleteCookieByKey('toast')
+        toast.success(message)
+        setHasShownToast(true)
       }
-    })
-  }, [])
+    })()
+  }, [hasShownToast])
 
   return null
 }
