664f1b7e4ee6f474afd26a8dc9ca4581813270fa
hhl206 <hhl206@qq.com>
2025-05-31 12:13:24 +0800

    feat: 添加头部动画效果，优化用户加载体验

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -20,12 +20,14 @@ const Header = () => {
     const fetchUser = async () => {
       const auth = await getAuth()
       setUser(auth.user)
+      setIsFetched(true)
     }
 
     fetchUser()
   }, [pathname])
 
   const [user, setUser] = useState<User | null>(null)
+  const [isFetched, setIsFetched] = useState(false)
 
   const navItems = user ? (
     <>
@@ -56,8 +58,12 @@ const Header = () => {
     </>
   )
 
+  if (!isFetched) {
+    return null
+  }
+
   return (
-    <nav className="supports-backdrop-blur:bg-background/60 bg-background/95 fixed left-0 right-0 top-0 z-20 flex w-full justify-between border-b px-5 py-2.5 backdrop-blur">
+    <nav className="supports-backdrop-blur:bg-background/60 bg-background/95 animate-header-from-top fixed left-0 right-0 top-0 z-20 flex w-full justify-between border-b px-5 py-2.5 backdrop-blur">
       <div className="flex items-center gap-x-2">
         <Link
           href={homePath()}
