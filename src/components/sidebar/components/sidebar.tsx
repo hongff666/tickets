b1f7ea32654c9daf77456e4315c96504c036a881
hhl206 <hhl206@qq.com>
2025-06-04 11:27:48 +0800

    feat: add sidebar animation and user authentication check in SideBar component

--- src/components/sidebar/components/sidebar.tsx
+++ src/components/sidebar/components/sidebar.tsx
@@ -1,11 +1,18 @@
 'use client'
 
+import { useAuth } from '@/features/auth/hooks/use-auth'
 import { cn } from '@/lib/utils'
 import { useState } from 'react'
 import { navItems } from '../constants'
 import { SidebarItem } from './sidebar-item'
 
 const SideBar = () => {
+  const { user, isFetched } = useAuth()
+
+  if (!user || !isFetched) {
+    return null
+  }
+
   const [isTransition, setIsTransition] = useState(false)
   const [isOpen, setIsOpen] = useState(false)
 
@@ -20,6 +27,7 @@ const SideBar = () => {
   return (
     <nav
       className={cn(
+        'animate-sidebar-from-left',
         'h-screen border-r pt-24',
         isTransition && 'duration-200',
         isOpen ? 'w-[78px] md:w-60' : 'w-[78px]',
