7ebc5e2513e2209df9770d7e6366247d1205d11e
hhl206 <hhl206@qq.com>
2025-06-06 22:33:41 +0800

    feat: add initial database migration and implement TicketSearchInput with enhanced search functionality

--- src/features/ticket/components/ticket-search-input.tsx
+++ src/features/ticket/components/ticket-search-input.tsx
@@ -1,8 +1,8 @@
 'use client'
 
 import { SearchInput } from '@/components/search-input'
-import { useQueryState } from 'nuqs'
-import { qParser } from '../search-params'
+import { useQueryState, useQueryStates } from 'nuqs'
+import { paginationOptions, paginationParser, qParser } from '../search-params'
 
 type TicketSearchInputProps = {
   placeholder?: string
@@ -11,5 +11,17 @@ type TicketSearchInputProps = {
 export const TicketSearchInput = ({ placeholder }: TicketSearchInputProps) => {
   const [q, setQ] = useQueryState('q', qParser)
 
-  return <SearchInput placeholder={placeholder} onChange={setQ} value={q} />
+  const [pagination, setPagination] = useQueryStates(
+    paginationParser,
+    paginationOptions,
+  )
+
+  const handleSearch = (value: string) => {
+    setQ(value)
+    setPagination({ ...pagination, page: 0 })
+  }
+
+  return (
+    <SearchInput placeholder={placeholder} onChange={handleSearch} value={q} />
+  )
 }
