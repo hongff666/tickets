8b4d0d6dba1016bbd8e9aa5b53979ddd246b4d4d
hhl206 <hhl206@qq.com>
2025-06-07 14:53:01 +0800

    feat: update GET endpoint to handle dynamic search parameters for ticket fetching

--- src/app/api/tickets/route.ts
+++ src/app/api/tickets/route.ts
@@ -1,12 +1,12 @@
 import { getTickets } from '@/features/ticket/queries/get-tickets'
+import { searchParamsCache } from '@/features/ticket/search-params'
 
-export async function GET() {
-  const { list, metadata } = await getTickets(undefined, {
-    page: 0,
-    size: 10,
-    sortKey: 'createdAt',
-    sortValue: 'desc',
-    q: '',
-  })
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url)
+  const untypedSearchParams = Object.fromEntries(searchParams)
+  const typedSearchParams = searchParamsCache.parse(untypedSearchParams)
+
+  const { list, metadata } = await getTickets(undefined, typedSearchParams)
+
   return Response.json({ list, metadata })
 }
