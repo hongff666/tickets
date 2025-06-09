364aad4816a3bd30d5809f9091e3c2f8bdaa7ca8
hhl206 <hhl206@qq.com>
2025-06-09 10:54:41 +0800

    fix: set default value for comments prop in Comments component

--- src/features/comment/components/comments.tsx
+++ src/features/comment/components/comments.tsx
@@ -10,7 +10,7 @@ type CommentsProps = {
   ticketId: string
   comments?: CommentWithMetadata[]
 }
-export const Comments = ({ ticketId, comments }: CommentsProps) => {
+export const Comments = ({ ticketId, comments = [] }: CommentsProps) => {
   return (
     <>
       <CardCompact
@@ -21,7 +21,7 @@ export const Comments = ({ ticketId, comments }: CommentsProps) => {
       />
 
       <div className="ml-10 flex flex-col gap-y-2">
-        {comments?.map((comment) => (
+        {comments.map((comment) => (
           <CommentItem
             key={comment.id}
             comment={comment}
